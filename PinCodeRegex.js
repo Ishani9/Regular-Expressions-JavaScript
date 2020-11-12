15aae236bef5b5cd8369c9621b6571eb92219b17
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-12 23:39:19 +0530

    Added: Functionality to Create a regex pattern and validate PIN Code

--- PinCodeRegex.js
+++ PinCodeRegex.js
@@ -1,1 +1,26 @@
-// PINCODE regex pattern
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+   
+  readline.question('What is your PIN CODE ? : ', PIN => {
+    console.log(`PIN IS : ` + PIN);
+    //calling test function
+    testForPinCode(PIN);
+    readline.close();
+  });
+  
+  //regex pattern
+const pincodeRegexPattern=RegExp('^[1-9]{1}[0-9]{5}$');
+
+//test function
+let testForPinCode=(PIN)=>{
+        if(pincodeRegexPattern.test(PIN))
+        {
+            console.log(`Given pincode ${PIN} IS VALID `);
+        }
+        else
+        console.log(`Given pincode ${PIN} IS INVALID. TRY AGAIN.... `);
+    }
+
