7daf432faa025d9824246b55b7e38adc9383187a
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-13 00:03:41 +0530

    Added: Functionality to Create a regex pattern and validate Email ID

---
+++ EmailRegex.js
@@ -0,0 +1,25 @@
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+   
+  readline.question('What is your EMAIL ? : ', email => {
+    console.log(`EMAIL IS : ` + email);
+    //calling test function
+    testForEmail(email);
+    readline.close();
+  });
+  
+  //regex pattern
+const emailRegexPattern = RegExp("^(abc[.][A-Za-z]+@bridgelabz[.]co[.][A-Za-z]{2,})$");
+
+//test function
+let testForEmail=(email)=>{
+        if(emailRegexPattern.test(email))
+        {
+            console.log(`Given EMAIL ID --> ${email} : IS VALID `);
+        }
+        else
+        console.log(`Given EMAIL ID --> ${email} : IS INVALID. TRY AGAIN.... `);
+    }
